f0086dc45898154891574d2d8e2ec097f34d5057
fryancisco <51490024+fryancisco@users.noreply.github.com>
2020-07-02 11:30:02 -0300

    Exercício #3
    Escrever um programa em C que leia 10 números inteiros positivos. Calcule e exiba a porcentagem de pares e de ímpares.

---
+++ #3.c
@@ -0,0 +1,15 @@
+#include <stdio.h> 
+int main(){ 
+  int par = 0, imp = 0, val, i;
+  for(i=0; i<=3; i++){
+    printf("Digite um numero: ");
+    scanf("%d", &val);
+    if(val % 2 == 0){
+      par++;
+    }else{
+      imp--;
+    }
+  }
+  printf("\n%d pares", par);
+  printf("\n%d impares", imp);
+}
